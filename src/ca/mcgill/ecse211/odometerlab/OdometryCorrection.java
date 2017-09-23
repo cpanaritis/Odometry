8e0c3951a8396ddd82efaf3bc2578ebc66b1efcc
kevinch8 <kevlawliet@gmail.com>
2017-09-23 16:38:04 -0400

    Attempt at correction

--- src/ca/mcgill/ecse211/odometerlab/OdometryCorrection.java
+++ src/ca/mcgill/ecse211/odometerlab/OdometryCorrection.java
@@ -14,6 +14,9 @@ public class OdometryCorrection extends Thread {
   private float scaledColor;
   private float[] LData;
   private double[] lastPosition;
+  private boolean[] update;
+  private boolean firstX = true;
+  private boolean firstY = true;
 
   // constructor
   public OdometryCorrection(Odometer odometer, SampleProvider colorSample) {
@@ -30,11 +33,53 @@ public class OdometryCorrection extends Thread {
       //TODO Place correction implementation here
       colorSample.fetchSample(LData, 0); // Get data from color sensor
       scaledColor = LData[0]*1000;
-      lastPosition = new double[3];
+      lastPosition = new double[2];
+      update = new boolean[2];
+      for(boolean data: update){
+    	  data = false;
+      }
       if (scaledColor <= 300) {	//makes a sound when its not a regular tile (very primitive only used
   	  	Sound.beep();		//for testing. If there's not enough data read for the line it will not beep.
-    }
-      System.out.println(scaledColor);
+  	  	if(odometer.getTheta() <= Math.PI/4){ //Update values for first side of square
+  	  		update[1] = true;				// want to change y values
+  	  		if(firstY = true){				// if first line of y then y = 0
+  	  			odometer.setY(0);
+  	  			firstY = false;
+  	  			odometer.getPosition(lastPosition, update);	// update our last position for next cycle
+  	  		}
+  	  		else{
+  	  			lastPosition[1] += OdometryLab.GRID_LENGTH;
+	  			odometer.setPosition(lastPosition, update); // update y value
+  	  		}
+  	  		update[1] = false;
+  	  	}
+  	  	else if(odometer.getTheta() <= 3*Math.PI/4){
+  	  		update[0] = true;			// want to change x values
+  	  		if(firstX = true){			// update values for 2nd side of square
+  	  			odometer.setX(0);		// if first line of x then x = 0
+  	  			firstX = false;
+  	  			odometer.getPosition(lastPosition, update);	
+  	  		}
+  	  		else{
+  	  			lastPosition[0] += OdometryLab.GRID_LENGTH;
+  	  			odometer.setPosition(lastPosition, update); // update x value
+  	  		}
+  	  		update[0] = false;
+  	  	}
+  	  	else if(odometer.getTheta() <= 5*Math.PI/4){ //3rd side of square
+  	  		update[1] = true;
+  	  		lastPosition[1] -= OdometryLab.GRID_LENGTH;
+			odometer.setPosition(lastPosition, update);
+			update[1] = false;
+  	  	}
+  	  	else{
+  	  		update[0] = true;
+  	  		lastPosition[0] -= OdometryLab.GRID_LENGTH;
+  	  		odometer.setPosition(lastPosition, update);
+  	  		update[0] = false;
+  	  	}
+  	  	
+      }      
       
       // this ensure the odometry correction occurs only once every period
       correctionEnd = System.currentTimeMillis();
