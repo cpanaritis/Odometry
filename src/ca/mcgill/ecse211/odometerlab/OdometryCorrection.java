5a0316befa3a1b519f97bd1b6fdd867a307cf58b
CPanaritis <christos.panaritis.mail.mcgill.ca>
2017-09-25 18:32:59 -0400

    updated correction

--- src/ca/mcgill/ecse211/odometerlab/OdometryCorrection.java
+++ src/ca/mcgill/ecse211/odometerlab/OdometryCorrection.java
@@ -13,12 +13,13 @@ public class OdometryCorrection extends Thread {
   private SampleProvider colorSample;
   private float scaledColor;
   private float[] LData;
-  private double[] lastPosition = new double[3];
-  private boolean[] update = new boolean[3];
   private boolean firstX = true;
   private boolean firstY = true;
   private boolean firstNegativeX = true;
   private boolean firstNegativeY = true;
+  private double lastX;
+  private double lastY;
+
 
 
 
@@ -37,54 +38,49 @@ public class OdometryCorrection extends Thread {
       //TODO Place correction implementation here
       colorSample.fetchSample(LData, 0); // Get data from color sensor
       scaledColor = LData[0]*1000;
-      for(boolean data: update){
-    	  data = false;
-      }
       
     	if (scaledColor <= 300) {	//makes a sound when its not a regular tile (very primitive only used
   	  	Sound.beep();		//for testing. If there's not enough data read for the line it will not beep.
   	  	if(odometer.getTheta() <= Math.PI/4){ //Update values for first side of square
-  	  		update[1] = true;				// want to change y values
   	  		if(firstY){	             		// if first line of y then y = 0
   	  			odometer.setY(0);
   	  			firstY = false;
-  	  			odometer.getPosition(lastPosition, update);	// update our last position for next cycle
+  	  			lastY=0;
   	  		}
   	  		else{
-  	  			lastPosition[1] += OdometryLab.GRID_LENGTH;
-	  			odometer.setY(lastPosition[1]); // update y value
+  	  			lastY += OdometryLab.GRID_LENGTH;
+	  			odometer.setY(lastY); // update y value
   	  		}
   	  	}
   	  	else if(odometer.getTheta() <= 3*Math.PI/4){
-  	  		update[0] = true;			// want to change x values
   	  		if(firstX){			// update values for 2nd side of square
   	  			odometer.setX(0);		// if first line of x then x = 0
   	  			firstX = false; 
-  	  			odometer.getPosition(lastPosition, update);	
+  	  			lastX = 0;
   	  		}
   	  		else{
-  	  			lastPosition[0] += OdometryLab.GRID_LENGTH;
-  	  			odometer.setX(lastPosition[0]); // update x value
+  	  			lastX += OdometryLab.GRID_LENGTH;
+  	  			odometer.setX(lastX); // update x value
   	  		}
   	  	}
   	  	else if(odometer.getTheta() <= 5*Math.PI/4){ //3rd side of square
   	  		if(firstNegativeY) {
-  	  			odometer.setY(lastPosition[1]);
+  	  			odometer.setY(lastY);
   	  			firstNegativeY = false;	
   	  		}
   	  		else {
-  	  		lastPosition[1] -= OdometryLab.GRID_LENGTH;
-			odometer.setY(lastPosition[1]);
+  	  		lastY -= OdometryLab.GRID_LENGTH;
+			odometer.setY(lastY);
   	  		}
   	  	}
   	  	else{
   	  		if(firstNegativeX) {
-  	  			odometer.setX(lastPosition[0]);
+  	  			odometer.setX(lastX);
   	  			firstNegativeX = false;
   	  		}
   	  		else {
-  	  		lastPosition[0] -= OdometryLab.GRID_LENGTH;
-  	  		odometer.setX(lastPosition[0]);
+  	  		lastX -= OdometryLab.GRID_LENGTH;
+  	  		odometer.setX(lastX);
   	  		}
   	  	}
   	  	
